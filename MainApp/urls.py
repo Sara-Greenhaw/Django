413f816980cfe98238b9b62ae2fa9f0c2ee8a5ca
Sara Greenhaw <Sara_Greenhaw1@baylor.edu>
2021-04-26 14:36:57 -0500

    updated

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from . import views 
 
 app_name = 'MainApp'
@@ -8,5 +7,19 @@ app_name = 'MainApp'
 #blank is the home page
 #access function called index in our views file
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), #homepage
+    path('topics', views.topics, name='topics'), #urls.py file
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    path('new_topic/', views.new_topic, name='new_topic'),
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
 ]
+
+
+        
+
+
+
+#anytime create new page, 
+# urls.py specify addy or location of webpage along w view of location --> urls in app folder pertaining to this particular app
+# views.py write funciton to interact w database
+#template html file to write code to display
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,8 +1,55 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 # Create your views here.
 def index(request):
     ''' The home page for learning log. '''
 
     return render(request, 'MainApp/index.html')
     #going to use index.html file and render it with data
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added') #sort order, default ascending, put minus for descending, all topics that are in database
+    context = {'topics':topics} #these must match, the value has to be whatever object passing to website, this is a dictionary to pass to website while loading
+
+    return render(request, 'MainApp/topics.html', context) #context of topics passed to topics.html
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_added') #descending order, getting from topic we have line above
+
+    context = {'topic':topic, 'entries':entries} #passing multiple object through context dictionary to website
+
+    return render(request, 'MainApp/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('MainApp:topics')
+    context = {'form':form}
+    return render(request, 'MainApp/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            #when we have new entry, don't have associative topic for that entry, don't know which particular topic is connected to entry
+            #this creates an instance called new entry with all information but false means dont save to database
+            #assign topic to it then save it
+            new_entry.topic = topic 
+            new_entry.save()
+            return redirect('MainApp:topic', topic_id=topic_id)
+
+    context = {'form':form, 'topic':topic}
+    return render(request, 'MainApp/new_entry.html', context)
